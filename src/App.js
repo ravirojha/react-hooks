cfb43ef6bd7ef61e2a84cd450b7088570f369c6b
Ravi <raviranjanojha777@gmail.com>
2021-07-28 13:31:28 +0530

    useReducer intro

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useFetch } from './useFetch';
-import { useForm } from './useForm';
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const [values, handleChange] = useForm({
-    email: '',
-    password: '',
-    firstName: ''
-  });
-
-  const [count, setCount] = useState(() =>
-    JSON.parse(localStorage.getItem('count'))
-  );
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-
-  useEffect(() => {
-    localStorage.setItem('count', JSON.stringify(count));
-  }, [count]);
-
-  // useEffect(() => {
-  //   const onMouseMove = (e) => {
-  //     console.log(e);
-  //   };
-
-  //   window.addEventListener('mousemove', onMouseMove);
-  //   return () => {
-  //     window.removeEventListener('mousemove', onMouseMove);
-  //   };
-  // }, []);
+  const [count, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
-      <div>{!data ? 'loading....' : data}</div>
       <div>count: {count}</div>
-      <button onClick={() => setCount((c) => c + 1)}>increment</button>
-      <input name="email" value={values.email} onChange={handleChange} />
-      <input
-        name="firstName"
-        placeholder="first name"
-        value={values.firstName}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>increment</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>decrement</button>
     </div>
   );
 };
