d7db10553a560593f2f165b9c83c0d4469cbfb0c
Ravi <raviranjanojha777@gmail.com>
2021-07-28 13:11:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useFetch } from './useFetch';
+import { useForm } from './useForm';
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: ''
+  });
+
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem('count'))
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem('count', JSON.stringify(count));
+  }, [count]);
+
+  // useEffect(() => {
+  //   const onMouseMove = (e) => {
+  //     console.log(e);
+  //   };
+
+  //   window.addEventListener('mousemove', onMouseMove);
+  //   return () => {
+  //     window.removeEventListener('mousemove', onMouseMove);
+  //   };
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{!data ? 'loading....' : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>increment</button>
+      <input name="email" value={values.email} onChange={handleChange} />
+      <input
+        name="firstName"
+        placeholder="first name"
+        value={values.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
